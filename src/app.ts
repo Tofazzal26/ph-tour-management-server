97bb70cce7f08c65aa425cb29137d2cdfa61381c
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-12-03 10:25:41 +0600

    user route and controller setup finish

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
-
+import { UserRoutes } from "./app/user/user.route";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use("/api/v1/user", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome to ph-tour-management" });
---
+++ src/app/user/user.controller.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.create({ name, email });
+    res
+      .status(httpStatus.CREATED)
+      .json({ message: "User Create Successfully", user });
+  } catch (error: any) {
+    console.log(error);
+    res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ message: `Something Went Wrong!! ${error.message}`, error });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
