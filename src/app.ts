c214f312e18847746da1c66117c8d04b00324747
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-12-04 11:58:50 +0600

    user index route dynamic validation finish

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from "express";
-import { UserRoutes } from "./app/user/user.route";
 import cors from "cors";
+import { router } from "./app/routes";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/v1/user", UserRoutes);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome to ph-tour-management" });
